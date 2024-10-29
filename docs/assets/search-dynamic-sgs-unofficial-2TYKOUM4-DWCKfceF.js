36f72b8da27caadb80888aa6aebb392da68d13c2
Pau Oliva <pof@eslack.org>
2024-10-29 18:03:29 +0100

    fix pages build

--- docs/assets/search-dynamic-sgs-unofficial-2TYKOUM4-DWCKfceF.js
+++ docs/assets/search-dynamic-sgs-unofficial-2TYKOUM4-DWCKfceF.js
@@ -1,4 +1,4 @@
-import{p as H}from"./chunk-Y3BVWVFU-DzU6t54e.js";import{s as J,J as N,R as y}from"./index-BA4_IlXs.js";var E=null;async function pB(){return E??(E=K())}async function K(){return H(await J.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
+import{p as H}from"./chunk-Y3BVWVFU-B6yTYMGb.js";import{s as J,J as N,R as y}from"./index-B7Yi1Qa6.js";var E=null;async function pB(){return E??(E=K())}async function K(){return H(await J.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
 
 Alg T
 Alg B
